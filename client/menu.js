22e30b2b8f2aef677a90a6e8795b433302c1ea1b
helb <helb@helb.cz>
2015-01-09 21:26:55 +0100

    display current route as active in menu; user name & logout

---
+++ client/menu.js
@@ -0,0 +1,16 @@
+Template.menu.helpers({
+  activeRoute: function(name) {
+    if(Router.current().route.getName() == name){
+      return true;
+    } else {
+      return false;
+    }
+  }
+});
+
+Template.menu.events({
+  'click #menu-logout': function(event) {
+    Meteor.logout();
+    Router.go('login');
+  }
+});
