b8cc06757bffc5d67c4999afa1d439fcf7ada2a0
helb <helb@helb.cz>
2015-01-09 17:59:26 +0100

    basic routing

---
+++ both/routes.js
@@ -0,0 +1,11 @@
+Router.route('/login', function () {
+  this.render('login');
+});
+
+Router.route('/chat', function () {
+  this.render('chat');
+});
+
+Router.route('/kalendar', function () {
+  this.render('kalendar');
+});
