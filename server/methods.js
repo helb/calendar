a66ec081f74873bd04ee8026b8afd037cd548acd
Jiří Helebrant <jiri.helebrant@nic.cz>
2015-01-13 14:28:19 +0100

    methods.js formatting

--- server/methods.js
+++ server/methods.js
@@ -1,10 +1,10 @@
 Meteor.startup(function() {
-  return Meteor.methods({
-    cleanMessages: function() {
-      return Messages.remove({});
-    },
-    cleanNotes: function() {
-      return Notes.remove({});
-    },
-  });
+    return Meteor.methods({
+        cleanMessages: function() {
+            return Messages.remove({});
+        },
+        cleanNotes: function() {
+            return Notes.remove({});
+        }
+    });
 });
