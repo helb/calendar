ba9c5c5870a7c53951876665301a32dcb91b884c
Jiří Helebrant <jiri.helebrant@nic.cz>
2015-01-13 13:47:39 +0100

    allow user to enable/disable their calendar column

--- client/user-settings.js
+++ client/user-settings.js
@@ -1,3 +1,15 @@
+Template.user_settings.helpers({
+    userHasCalendar: function(userId) {
+        return Meteor.users.find({
+            _id: userId
+        }).fetch()[0].profile.calendar;
+    }
+});
+
+Template.user_settings.rendered = function() {
+    $("input.checked").prop('checked', true);
+}
+
 Template.user_settings.events({
     'click .btn-change-color': function(event) {
         var newColor = event.currentTarget.classList[1];
@@ -8,5 +20,15 @@ Template.user_settings.events({
                 "profile.color": newColor
             }
         });
-    }
+    },
+    'change #check-calendar': function(event) {
+        var isChecked = event.currentTarget.checked;
+        Meteor.users.update({
+            _id:  Meteor.user()._id
+        }, {
+            $set: {
+                "profile.calendar": isChecked
+            }
+        });
+    },
 });
